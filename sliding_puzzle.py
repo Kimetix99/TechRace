35bf227893766905d1a0267cd2e7afa3b1baaace
Horno <i.palacin.a@gmail.com>
2019-09-29 12:41:28 +0200

    Minor commit

---
+++ sliding_puzzle.py
@@ -0,0 +1,23 @@
+
+class Node:
+    def __init__(self, state, path):
+        self.state = state
+        self.path = path
+
+
+def nextmove(state):
+    list_state =list(state)
+    for i, coord in enumerate(state):
+        if i == 0:
+            
+
+
+def solution():
+    starting_state = (6,7,5,
+                      1,8,2,
+                      4,3,0
+                      )
+    
+
+
+solution()
